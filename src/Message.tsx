d0b0da4221d0d2d6949f1a7948915ea2076517fd
Shubham Meshram <meshram.shubham27@gmail.com>
2025-02-04 18:40:51 +0530

    basic component

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// Pascal casing
+function Message() {
+    // JSX: Javascript XML
+    const name = 'Happy'
+    if (name)
+        return <h1>Hello {name} World</h1>;
+    return <h1>Hello Dummy World</h1>;
+}
+
+export default Message;
